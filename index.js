52ba064af4f8d08fc40df50245ad8ec6a8c4c48d
adonis <robertoalbeldac@gmail.com>
2019-01-20 17:39:40 -0800

    dad

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ var mysql = require('mysql');
 var about = require('./routes/about');
 
 //database connection
-var connection = mysql.createPool({
-	connectionLimit: 50,
-	host: 'localhost',
-	user: 'root',
-	password : '',
-	database: 'hotel'
+// var connection = mysql.createPool({
+// 	connectionLimit: 50,
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password : '',
+// 	database: 'hotel'
 
-});
+// });
 
 // connection.connect(function(error){
 // 	if(!!error) {
@@ -53,24 +53,24 @@ app.get('/students', function(req, res){
 	res.render("students", {name : "hello world"});	
 });
 
-app.get('/login', function(req, res){
-	connection.getConnection(function(error, tempCount){
-		if (!!error){
-			tempCount.release();
-			console.log("error in the query");
-		} else {
-			console.log("query successed");
-			tempCount.query("SELECT * FROM gas", function(error, rows, fields){
-				tempCount.release();
-				if (!!error){
-					console.log('Error in the query');
-				} else {
-					res.render("login", {value: rows[0]});
-				}		
-			});
-		}
-	});
-});
+// app.get('/login', function(req, res){
+// 	connection.getConnection(function(error, tempCount){
+// 		if (!!error){
+// 			tempCount.release();
+// 			console.log("error in the query");
+// 		} else {
+// 			console.log("query successed");
+// 			tempCount.query("SELECT * FROM gas", function(error, rows, fields){
+// 				tempCount.release();
+// 				if (!!error){
+// 					console.log('Error in the query');
+// 				} else {
+// 					res.render("login", {value: rows[0]});
+// 				}		
+// 			});
+// 		}
+// 	});
+// });
 
 app.get('/home', function(req, res){
 	res.render("home", {name : "hello world"});	
