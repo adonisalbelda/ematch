8c04428243e5d68a3bfd92e68bcfda6047bb2f8d
adonis <robertoalbeldac@gmail.com>
2019-02-26 23:06:55 -0800

    upload with database

--- assets/js/websocket.js
+++ assets/js/websocket.js
@@ -1,4 +1,5 @@
-var socket = io.connect('http://localhost:3000');
+// var socket = io.connect('http://localhost:3000');
+var socket = io.connect('https://e-match.herokuapp.com/');
 
 socket.emit('send message', "dasdad");
 
