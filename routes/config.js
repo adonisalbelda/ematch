1c83dc8df10d98f0c351a2132fb6efae9393b4cd
adonis <robertoalbeldac@gmail.com>
2019-02-27 00:39:16 -0800

    upload with database

--- routes/config.js
+++ routes/config.js
@@ -1,21 +1,21 @@
 var mysql = require('mysql');
 
-var connection = mysql.createPool({
-	connectionLimit: 50,
-	host: 'localhost',
-	user: 'root',
-	password : '',
-	database: 'db_game_skills'
-
-});
-
 // var connection = mysql.createPool({
 // 	connectionLimit: 50,
-// 	host: 'remotemysql.com',
-// 	user: 'U7jBmzPtGd',
-// 	password : 'M7g5bEFE9X',
-// 	database: 'U7jBmzPtGd'
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password : '',
+// 	database: 'db_game_skills'
 
-// });	
+// });
+
+var connection = mysql.createPool({
+	connectionLimit: 50,
+	host: 'remotemysql.com',
+	user: 'U7jBmzPtGd',
+	password : 'M7g5bEFE9X',
+	database: 'U7jBmzPtGd'
+
+});	
 
 module.exports = connection;
